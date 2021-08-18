7c8c2dee6dc9082e16cdad31018c0efc180fefbd
joseph-l-o <74804074+Joseph-L-O@users.noreply.github.com>
2021-08-18 16:05:42 -0300

    ignews

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { session, signin } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -5,10 +8,30 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+
+    async function handleSubscribe(){
+        if(!session){
+            signin('github')
+            return;
+        }
+        try{
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId:sessionId})
+        }catch (err){
+            alert(err.message)
+        }
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe
         </button>
