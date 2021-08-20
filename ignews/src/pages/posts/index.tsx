9a0ee3b24bee47a8304c7cffeca09214d6cc7842
joseph-l-o <74804074+Joseph-L-O@users.noreply.github.com>
2021-08-20 15:34:58 -0300

    ignews finalizada

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+import Link from 'next/link';
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+interface Posts {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: Posts) {
     return (
         <>
             <Head>
@@ -12,38 +24,44 @@ export default function Posts() {
             </Head>
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de agosto de 2021</time>
-                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eos distinctio quia pariatur iusto libero harum! Similique labore fuga aspernatur cum laudantium tempore quibusdam suscipit nulla minima consequuntur! Aut, ducimus!</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de agosto de 2021</time>
-                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eos distinctio quia pariatur iusto libero harum! Similique labore fuga aspernatur cum laudantium tempore quibusdam suscipit nulla minima consequuntur! Aut, ducimus!</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de agosto de 2021</time>
-                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eos distinctio quia pariatur iusto libero harum! Similique labore fuga aspernatur cum laudantium tempore quibusdam suscipit nulla minima consequuntur! Aut, ducimus!</p>
-                    </a>
+                    {posts.map(post => (
+                        <Link href={`/posts/${post.slug}`}>
+                            <a key={post.slug}>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+                    ))}
                 </div>
             </main>
         </>
     );
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//     const prismic = getPrismicClient()
-//     const response = await prismic.query([
-//         Prismic.predicates.at('document.type', 'posts')
-//     ], {
-//         fetch: ['publication.title', 'publication.content'],
-//         pageSize: 100
-//     })
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'posts')
+    ], {
+        fetch: ['publication.title', 'publication.content'],
+        pageSize: 100
+    })
 
-//     console.log(response)
-//     return {
-//         props: {}
-//     }
-// }
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+
+    return {
+        props: { posts }
+    }
+}
